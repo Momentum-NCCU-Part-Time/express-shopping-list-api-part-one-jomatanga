5ccd529be6e6d066f4abddab773f58a104f577bf
jomatanga <jomatanga11@gmail.com>
2024-02-01 16:34:06 -0500

    Start

---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express()
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://jomatanga11:Gen3226@cluster0.wls3bt9.mongodb.net/local_library?retryWrites=true&w=majority";
+
+app.listen(3000, () => {
+
+}) 
